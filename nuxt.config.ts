f381d4ae43784dec798dd876ae0e9f36c04838a3
Rakib Ul Hasan <rakib@Rakibs-Mac-mini.local>
2025-09-04 07:27:39 +0600

    🚨 added github page deploymenty workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,12 @@
 const config = require('./developer.json')
 const siteTitle = `${config.name} | ${config.role}`
 
-
-/*
- * Nuxt 3 Config File
- Usage: https://nuxt.com/docs/api/configuration/nuxt-config
- */
 export default defineNuxtConfig({
-
-  /**
-   * * App Config
-   * app config: https://nuxt.com/docs/api/configuration/nuxt-config#app
-   * head config: https://nuxt.com/docs/api/configuration/nuxt-config#head
-   * meta config: https://nuxt.com/docs/getting-started/seo-meta
-   * pageTransition config: https://nuxt.com/docs/getting-started/transitions#transitions
-   * TODO: Add more meta tags for SEO
-   * TODO: Add tags for social media sharing
-   * TODO: Migrate apple-touch-icon config to manifest.json
-   */
   app: {
+    // Fallback base URL if NUXT_APP_BASE_URL isn't set
+    baseURL: process.env.NUXT_APP_BASE_URL || '/portfolio-2.0/',
     head: {
-      title: siteTitle, // App window nav title
+      title: siteTitle,
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -28,9 +14,8 @@ export default defineNuxtConfig({
         { hid: 'og:title', property: 'og:title', content: siteTitle },
         { hid: 'og:description', property: 'og:description', content: 'A awesome developer portfolio design.' },
         { hid: 'og:image', property: 'og:image', content: 'demo-share.jpg' },
-        { hid: 'og:url', property: 'og:url', content: 'https://developer-portfolio-v1.netlify.app/' },
+        { hid: 'og:url', property: 'og:url', content: 'https://rakibulhasan-111.github.io/portfolio-2.0/' },
         { name: 'theme-color', content: '#010C15' },
-        // ...
       ],
       link: [
         { rel: 'manifest', href: 'pwa/manifest.json' },
@@ -38,45 +23,27 @@ export default defineNuxtConfig({
       ],
     },
   },
-
-  /**
-   * * Nuxt 3 Modules
-   * Official modules: https://nuxt.com/modules
-   */
+  nitro: {
+    preset: 'github_pages',
+  },
   modules: [
     '@nuxtjs/tailwindcss',
   ],
-
   components: {
-    dirs: [
-      '~/components',
-    ],
+    dirs: ['~/components'],
   },
-  
-  /**
-   * * Tailwind CSS Config
-   * Options: https://tailwindcss.nuxt.dev/getting-started/options/
-   * Docs: https://tailwindcss.nuxt.dev
-   */
   tailwindcss: {
     cssPath: '~/assets/tailwind.css',
     configPath: 'tailwind.config',
-    exposeConfig: true, // true to resolve the tailwind config in runtime. https://tailwindcss.nuxt.dev/getting-started/options/#exposeconfig
+    exposeConfig: true,
     injectPosition: 0,
     viewer: false,
   },
-
-  /**
-   * * Runtime Config (Environment Variables)
-   * Usage: https://nuxt.com/docs/guide/going-further/runtime-config
-   */
   runtimeConfig: {
-    // The private keys which are only available server-side
     apiSecret: '123',
-    // Keys within public are also exposed client-side
     public: {
       apiBase: '/api',
-      dev: config
-    }
-  }
+      dev: config,
+    },
+  },
 })
